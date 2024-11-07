9aa30da43e20c40252a4931733c784d1fba35204
mudassirl6 <mudassirahmeddvk@gmail.com>
2024-11-07 15:37:48 +0530

    STEVES0001 - Wrote the code for finding the area of the circle

---
+++ src/main/java/com/example/javamavenjunithelloworld/utility.java
@@ -0,0 +1,11 @@
+package main.java.com.example.javamavenjunithelloworld;
+public class utility {
+    public static void main(String[] args) {
+        System.out.println("Hi I am Mudassir");
+        float a = 123f;
+        float b = 123f;
+        System.out.println(a+b);
+
+    }
+    
+}
